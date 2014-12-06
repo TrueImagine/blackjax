7b520a12bcc38224bcc813c8757abbab4a39a585
TrueImagine <imagin3.999@gmail.com>
2014-12-07 00:23:02 +0700

    Database change

--- klik_toys.sql
+++ klik_toys.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 06, 2014 at 04:12 PM
+-- Generation Time: Dec 06, 2014 at 04:47 PM
 -- Server version: 5.6.20
 -- PHP Version: 5.5.15
 
