86ba9bf098197042a43f68b0983b4bec0cf57f92
andrewnugraha <rafael012896@gmail.com>
2014-12-06 22:46:11 +0700

    membuat signup_user.php

---
+++ public/singup_user.php
@@ -0,0 +1,16 @@
+<!-- signup_user -->
+<?php
+
+
+
+
+?>
+<form action="singup_user_proses.php" method="POST">
+Username:
+<input type="text" name="username" /><br />
+Password:
+<input type="password" name="password" /><br />
+Re-Type Pasword:
+<input type="password" name="password" /><br />
+<input type="submit" name="singup" value="Signup" />
+</form>
