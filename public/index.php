ab91d2efbd549bdf4c9bfd1cde3b7556b04f31fc
TrueImagine <imagin3.999@gmail.com>
2014-12-06 21:11:39 +0700

    Index.php
    dasar halaman utama

---
+++ public/index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Kliktoys.com | Your one stop shop to games and toys!</title>
+		<link rel="stylesheet" type="text/css" href="style/toystyle.css" />
+	</head>
+	<body>
+		<!-- header berisi banner(gambar) dan top navigation(navi) -->
+		<div id="header">
+			<img src="" placeholder="Kliktoys Banner" />
+			
+			<!-- navigation bar -->
+			<ul id="navi">
+				<li><a href="index.php">Home</a></li>
+				<li><a href="products.php">Browse Products</li>
+				<li><a href="about_us.php">About us</li>
+				<li><a href="about_us.php">Contact us</li>
+			</ul>
+		</div><!-- end of header -->
+		
+		<!-- left_wing berisi link-link ke produk sesuai kategorinya,
+			daftar link bisa di edit di bagian kategori barang -->
+		<div id="left_wing">
+			<ul>
+				<li>SH Figuarts</li>
+				<li>SIC</li>
+				<li>SCM</li>
+				<li>Nendoroid</li>
+			</ul>
+		</div><!-- end of left-wing -->
+		
+		<!-- content menampilkan news dan lini produk terbaru -->
+		<div id="content">
+			<!-- bagian news, isi news bisa ditambah, edit, dan delete di database -->
+			<div id="news">
+				<h2>Latest news</h2>
+				<!-- isi dengan news di database -->
+			</div><!-- end of news -->
+			
+			<!-- bagian product-line menampilkan beberapa produk. Jumlah produk yang ditampilkan
+			dan aturannya tergantung dari rules php -->
+			<div id="product-line">
+				<!-- cetak berdasarkan rules php -->
+			</div>
+		</div>
+		
+		<!-- footer menampilkan... -->
+		<div id="footer">
+			<!-- isi footer pikirin nanti -->
+		</div>
+	</body>
+</html>
