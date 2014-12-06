4f5d037fd35cc0d5a060d362fee1b301cacf8edf
andrewnugraha <rafael012896@gmail.com>
2014-12-06 22:12:57 +0700

    update

---
+++ public/login_admin.php
@@ -0,0 +1,13 @@
+<!-- login.php -->
+<?php
+?>
+
+<!-- form berisi username dan password untuk login -->
+<form action="login_admin_proses.php" method="POST">
+Username:
+<input type="text" name="username" /><br />
+Password:
+<input type="password" name="password" /><br />
+<input type="submit" name="login" value="Login" />
+</form>
+<!-- End untuk form -->
