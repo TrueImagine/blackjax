9711234bf0677c84d2758f71c0866a62e7039f2b
andrewnugraha <rafael012896@gmail.com>
2014-12-08 18:03:28 +0700

    ganti sedikit

--- public/contact_us.php
+++ public/contact_us.php
@@ -1,9 +1,22 @@
 <!-- contact_us.php -->
-<form action="contact_us_proses.php" method="POST">
-
-Email:
-<input type="text" name="email" /><br />
-Kritik dan saran:
-<textarea name="krisan" rows="4"></textarea><br />
-<input type="submit" name="Submit" value="Submit" />
+<?php
+	session_start();
+	include("../includes/connection.php");
+?>
+<html>
+<head>
+		<title>Kliktoys.com | Your one stop shop to games and toys!</title>
+		<link rel="stylesheet" type="text/css" href="style/toystyle.css" />
+		<link rel="stylesheet" type="text/css" href="style/productstyle.css" />
+</head>
+<body>	
+		<form action="contact_us_proses.php" method="POST">
+		Email:
+		<input type="text" name="email" /><br />
+		Kritik dan saran:
+		<textarea name="krisan" rows="4"></textarea><br />
+		<input type="submit" name="Submit" value="Submit" />
 </form>
+
+</body>
+</html>
