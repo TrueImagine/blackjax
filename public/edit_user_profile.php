6b5ed2ac7668807f01d59be9cca2f5a5dc58d18d
andrewnugraha <rafael012896@gmail.com>
2014-12-11 00:50:25 +0700

    rapihi edituser

--- public/edit_user_profile.php
+++ public/edit_user_profile.php
@@ -1,19 +1,54 @@
-<!-- edit_user_profile.php -->
- <?php
+<!-- index.php -->
+<?php
 	session_start();
 	include("../includes/connection.php");
 	include("../includes/functions.php");
- ?>
+?>
+
+<!DOCTYPE html>
 <html>
-
-<head>
-	<title>Kliktoys.com | Your one stop shop to games and toys!</title>
-	<!-- <link rel="stylesheet" type="text/css" href="style/toystyle.css" />
-	<link rel="stylesheet" type="text/css" href="style/productstyle.css" /> -->
-</head>
-
-<body>
-	<h1>EDIT PROFILE: </h1>
+	<head>
+		<title>Kliktoys.com | Your one stop shop to games and toys!</title>
+		<link rel="stylesheet" type="text/css" href="style/toystyle.css" />
+		<link rel="stylesheet" type="text/css" href="style/productstyle.css" />
+	</head>
+	<body>
+		<!-- div body menandakan container isi (header,content,dsb) dari dokumen web,
+			ukuran body membuat web tidak mengambil layar secara penuh -->
+		<div id= "body">
+			<!-- header berisi banner(gambar) dan top navigation(navi) -->
+			<!-- bagian header dimasukkan ke header.php -->
+			<?php include("../includes/header.php");?>
+			
+			<!-- left_wing berisi search, mini-cart dan link-link ke produk sesuai kategorinya,
+				daftar link bisa di edit di bagian kategori barang -->
+			<!-- bagian left-wing dimasukkan ke left_wing.php -->
+			<?php include("../includes/left_wing.php")?>
+			
+			<!-- main-content menampilkan message, news dan lini produk terbaru -->
+			<div id="main-content">
+				<!-- Jika ada message di session, tampilkan pesan -->
+				<!-- container untuk pesan ada di pesan.php -->
+				<?php cetakPesan(); ?>
+				<!-- bagian news, isi news bisa ditambah, edit, dan delete di database -->
+				<div id="news">
+					<h2>Latest news</h2>
+					<?php
+						$query = "SELECT * FROM news LIMIT 3";
+						$tabel_berita = mysqli_query($connection,$query);
+						
+						while($baris = mysqli_fetch_assoc($tabel_berita)){
+							echo "<h4>";
+							echo $baris['tanggal'];
+							echo "</h4>";
+							echo "<p>";
+							echo $baris['isi'];
+							echo "</p>";
+						}
+					?>
+				</div><!-- end of news -->
+				
+				<h1>EDIT PROFILE: </h1>
 	<?php
 		$sql="SELECT * from reg_user WHERE nama='$_SESSION[reg_user]'";
 		$hasil = mysqli_query($connection, $sql);
@@ -35,7 +70,7 @@
 		echo "<br>";
 		echo "<br>";
 	?>
-	<?php include("../includes/pesan.php"); ?>
+	<?php cetakPesan(); ?>
 	<form action="edit_user_profile_proses.php" method="POST" >
 		Password:
 		<input type="password" name="password" /><br />
@@ -49,6 +84,17 @@
 		<input type="text" name="telp" /><br />
 		<input type="submit" name="signup" value="Signup" />
 	</form>
-</body>
-
+			</div>
+			<p class="clearFloat"></p>
+			
+			<!-- footer menampilkan... -->
+			<div id="footer">
+				Copyright <?php echo date("Y"); ?>, www.kliktoys.com
+			</div>
+		</div><!-- end of body -->
+		<?php
+			//tutup koneksi setelah page selesai di load
+			mysqli_close($connection);
+		?>
+	</body>
 </html>
--- public/edit_user_profile_proses.php
+++ public/edit_user_profile_proses.php
@@ -11,7 +11,7 @@
 		$hash = "JaxJaxJaxJaxJaxJax2222";
 		$salt = $format.$hash;
 		$newpass = crypt($password,$salt);
-		$update="UPDATE reg_user SET nama='$_SESSION[reg_user]' ,enc_pass='$newpass' ,alamat='$_POST[alamat]' ,email='$_POST[email]' ,telepon='$_POST[telp]'";
+		$update="UPDATE reg_user SET enc_pass='$newpass' ,alamat='$_POST[alamat]' ,email='$_POST[email]' ,telepon='$_POST[telp]' WHERE id='$_SESSION[reg_user]'";
 		$query=mysqli_query($connection,$update);
 		$_SESSION['message']="Profile Berhasil di Update!";
 		header('Location:edit_user.php');	
